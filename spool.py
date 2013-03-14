bacf80d7a94b5b21d1f803a0dd75e54c518655dd
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-03-14 22:23:40 +0100

    Bind a queue to coroutines for message passing
    Example:
    
        @coroutine
        def echoargs(*args):
            chan = coroutine.self()
            chan.put(args)

--- spool.py
+++ spool.py
@@ -1,12 +1,19 @@
-from threading import Thread
+from threading import Thread, current_thread
 from multiprocessing import Queue
+
 
 def coroutine(fun):
     def replacement(*args):
         return (fun, args)
     return replacement
 
+coroutine.self = lambda: current_thread().__chan__
+
 def go(spec):
     fun, args = spec
-    Thread(target=fun, args=args).start()
+    t = Thread(target=fun, args=args)
+    q = Queue()
+    t.__setattr__('__chan__', q)
+    t.start()
+    return q
 
