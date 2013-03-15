35e287318011395978bc566c346e4971c18fe24e
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-03-15 21:52:57 +0100

    Implement Channel as an API to talk to a coroutine
    This allow us to not mess with queues endpoints

--- spool.py
+++ spool.py
@@ -1,8 +1,27 @@
 from threading import Thread, current_thread
-from multiprocessing import Queue
+import multiprocessing
+from select import select as unix_select
+from time import sleep
+
+class Channel(object):
+
+    def __init__(self):
+        self._in = multiprocessing.Queue()
+        self._out = multiprocessing.Queue()
+
+    def incoroutine(self):
+        return coroutine.self() is self
+
+    def get(self):
+        q = self._in if self.incoroutine() else self._out
+        return q.get()
+
+    def put(self, *args):
+        q = self._out if self.incoroutine() else self._in
+        return q.put(*args)
 
 def go(fun):
-    chan = Queue()
+    chan = Channel()
 
     def _fun(*args):
         t = Thread(target=fun, args=args)
