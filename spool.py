1f2bd2015692037e35b144cc7eec16850d185b86
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-03-15 21:56:32 +0100

    Implement select to wait for async channel IO

--- spool.py
+++ spool.py
@@ -3,6 +3,14 @@ import multiprocessing
 from select import select as unix_select
 from time import sleep
 
+def alive(source):
+    try:
+        source.fileno()
+        return True
+    except:
+        return False
+
+
 class Channel(object):
 
     def __init__(self):
@@ -20,6 +28,15 @@ class Channel(object):
         q = self._out if self.incoroutine() else self._in
         return q.put(*args)
 
+    def fileno(self):
+        q = self._in if self.incoroutine() else self._out
+        return q._reader.fileno()
+
+    def close(self):
+        self._in.close()
+        self._out.close()
+
+
 def go(fun):
     chan = Channel()
 
@@ -33,4 +50,13 @@ def go(fun):
 
 coroutine = go
 coroutine.self = lambda: current_thread().__chan__ if hasattr(current_thread(), '__chan__') else None
+
+
+def select(*sources):
+    sources = filter(alive, sources)
+    while sources:
+        readables, writables, exceptions = unix_select(sources, [], [])
+        for source in readables:
+            yield source
+        sources = filter(alive, sources)
 
